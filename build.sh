57860b595b6b93ecc27b7a4671177cbd2ba48dab
Alingof <Alignof@outlook.com>
2022-08-07 19:47:50 +0900

    [fix] fix listing text color

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 pandoc -d build_config.yaml &&\
     lualatex -shell-escape tmp.tex &&\
     lualatex -shell-escape tmp.tex &&\
-    mv tmp.pdf 17423.pdf &&\
+    mv tmp.pdf 202213581.pdf &&\
     rm -rf tmp* *.log
